eb4f9db7bc45232e015218a2d9339c76b780464f
Дмитрий <dmsekot@yandex.ru>
2023-10-25 12:22:15 +0300

    [feature] Исправляет импорты

--- src/app/app-router.tsx
+++ src/app/app-router.tsx
@@ -1,7 +1,7 @@
 import { Navigate, createBrowserRouter } from "react-router-dom";
 
-import { Home } from "../pages/home";
-import { Login } from "../pages/login";
+import { Home } from "@pages/home";
+import { Login } from "@pages/login";
 
 import Layout from "./layout";
 
